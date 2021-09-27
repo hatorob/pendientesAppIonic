4aea1c6c22192b861c17c447e41d5e62b983b620
Hector Toro <alejotoro94@hotmail.com>
2021-09-27 12:20:55 -0500

    Se creo metodo para guardar información en el LocalStorage y otro para cargar la información en la APP del LocalStorage

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,15 +8,35 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() { 
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Heroes a desaparecer');
-    this.listas.push(lista1, lista2);
-    console.log(this.listas); 
+    this.cargarStorage();
+    
   }
 
 
   crearLista( titulo: string) {
     const nuevaLista = new Lista(titulo);
     this.listas.push( nuevaLista );
+    this.guardarStorage();
   }
+
+
+//LocalStorage
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+  
+  // Cuando la aplicación carga por primera vez, debe validar si hay información el el stiorae y cargarla
+  cargarStorage() {
+    
+    if ( localStorage.getItem('data') ){
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+
+      this.listas = [];
+    }
+  }
+
 }
+
+
